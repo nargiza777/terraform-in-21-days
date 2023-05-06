5800a1345bcc383b94bc9b75aa3afea43521a029
NargizaOsmon <nargizaosmon@gmail.com>
2023-05-06 13:57:43 -0500

    Adding terraform fmt

--- vpc.tf
+++ vpc.tf
@@ -11,25 +11,25 @@ resource "aws_vpc" "main" {
 }
 
 resource "aws_subnet" "public" {
-  count = length(var.public_cidr)
-  vpc_id     = aws_vpc.main.id
-  cidr_block = var.public_cidr[count.index]
+  count             = length(var.public_cidr)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.public_cidr[count.index]
   availability_zone = local.availability_zones[count.index]
 
   tags = {
-    Name = "${var.env_code}-public${count.index+1}"
+    Name = "${var.env_code}-public${count.index + 1}"
   }
 }
 
 
 resource "aws_subnet" "private" {
-  count = length(var.private_cidr)
-  vpc_id     = aws_vpc.main.id
-  cidr_block = var.private_cidr[count.index]
+  count             = length(var.private_cidr)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.private_cidr[count.index]
   availability_zone = local.availability_zones[count.index]
 
   tags = {
-    Name = "${var.env_code}-private${count.index+1}"
+    Name = "${var.env_code}-private${count.index + 1}"
   }
 }
 
@@ -57,7 +57,7 @@ resource "aws_route_table" "public" {
 
 
 resource "aws_route_table_association" "public" {
-  count = length(var.public_cidr)
+  count          = length(var.public_cidr)
   subnet_id      = aws_subnet.public[count.index].id
   route_table_id = aws_route_table.public.id
 }
@@ -65,20 +65,20 @@ resource "aws_route_table_association" "public" {
 
 resource "aws_eip" "nat" {
   count = length(var.public_cidr)
-  vpc      = true
+  vpc   = true
 
   tags = {
-    Name = "${var.env_code}-nat${count.index+1}"
+    Name = "${var.env_code}-nat${count.index + 1}"
   }
 }
 
 resource "aws_nat_gateway" "main" {
-  count = length(var.private_cidr)
+  count         = length(var.private_cidr)
   allocation_id = aws_eip.nat[count.index].id
   subnet_id     = aws_subnet.public[count.index].id
 
   tags = {
-    Name = "${var.env_code}-main${count.index+1}"
+    Name = "${var.env_code}-main${count.index + 1}"
   }
 
   # To ensure proper ordering, it is recommended to add an explicit dependency
@@ -88,21 +88,21 @@ resource "aws_nat_gateway" "main" {
 
 
 resource "aws_route_table" "private" {
-  count = length(var.private_cidr)
+  count  = length(var.private_cidr)
   vpc_id = aws_vpc.main.id
 
   route {
-    cidr_block = "0.0.0.0/0"
+    cidr_block     = "0.0.0.0/0"
     nat_gateway_id = aws_nat_gateway.main[count.index].id
   }
 
   tags = {
-    Name = "${var.env_code}-private${count.index+1}"
+    Name = "${var.env_code}-private${count.index + 1}"
   }
 }
 
 resource "aws_route_table_association" "private" {
-  count = length(var.private_cidr)
+  count          = length(var.private_cidr)
   subnet_id      = aws_subnet.private[count.index].id
   route_table_id = aws_route_table.private[count.index].id
 }
