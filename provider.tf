7e817504f423e0d1f10a1b01f73229ed1adaaafd
NargizaOsmon <nargizaosmon@gmail.com>
2023-05-10 14:06:46 -0500

    Configure remote state

--- provider.tf
+++ provider.tf
@@ -1,3 +1,11 @@
+terraform {
+  backend "s3"{
+    bucket = "terraform-remote-state-123"
+    key = "terraform.state"
+    region = "us-east-1"
+  }
+}
+
 provider "aws" {
   region = "us-east-1"
 }
