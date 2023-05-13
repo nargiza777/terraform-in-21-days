7cd97aea519c9994e8873ca668c59188d7d8c37d
NargizaOsmon <nargizaosmon@gmail.com>
2023-05-12 21:13:43 -0500

    Split terraform state into multiple layers

---
+++ level2/instance.tf
@@ -0,0 +1,97 @@
+data "aws_ami" "amazonlinux" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-kernel-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+resource "aws_instance" "public" {
+  ami                         = data.aws_ami.amazonlinux.id
+  associate_public_ip_address = true
+  instance_type               = "t3.micro"
+  key_name                    = "ssh-may"
+  vpc_security_group_ids      = [aws_security_group.public.id]
+  subnet_id                   = data.terraform_remote_state.level1.outputs.public_subnet_id[0]
+  user_data                   = file("userdata.sh")
+
+  tags = {
+    Name = "${var.env_code}-public"
+  }
+}
+
+resource "aws_security_group" "public" {
+  name        = "${var.env_code}-piblic"
+  description = "Allow inbound traffic"
+  vpc_id      = data.terraform_remote_state.level1.outputs.vpc_id
+
+  ingress {
+    description = "SSH from public"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["98.206.24.195/32"]
+  }
+
+  ingress {
+    description = "HTTP from public"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.env_code}-public"
+  }
+}
+
+resource "aws_instance" "private" {
+  ami                    = data.aws_ami.amazonlinux.id
+  instance_type          = "t3.micro"
+  key_name               = "ssh-may"
+  vpc_security_group_ids = [aws_security_group.public.id]
+  subnet_id              = data.terraform_remote_state.level1.outputs.private_subnet_id[0]
+
+  tags = {
+    Name = "${var.env_code}-public"
+  }
+}
+
+resource "aws_security_group" "private" {
+  name        = "${var.env_code}-private"
+  description = "Allow VPC traffic"
+  vpc_id      = data.terraform_remote_state.level1.outputs.vpc_id
+
+  ingress {
+    description = "SSH from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [data.terraform_remote_state.level1.outputs.vpc_cidr]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.env_code}-private"
+  }
+}
