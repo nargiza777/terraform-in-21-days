c8c99f4e977610be19680d60cbe8c846b24797c9
NargizaOsmon <nargizaosmon@gmail.com>
2023-05-20 14:41:13 -0500

    Add ACM Certificate

--- provider.tf
+++ provider.tf
@@ -1,8 +1,8 @@
 terraform {
-  backend "s3"{
-    bucket = "terraform-remote-state-123"
-    key = "terraform.state"
-    region = "us-east-1"
+  backend "s3" {
+    bucket         = "terraform-remote-state-123"
+    key            = "terraform.state"
+    region         = "us-east-1"
     dynamodb_table = "terraform-remote-state"
   }
 }
